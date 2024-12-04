231ffd639c43ad15d295e0c34a7063e77677c324
Hafsa0987 <hafsasaghir06@gmail.com>
2024-12-03 21:09:46 -0800

    Add files via upload

---
+++ lab 12 task 3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#include<cstring>
+int main()
+{
+	string str="Hello, World";
+	int length=str.length();
+	cout<<"The length of a string is"<<length<<endl;
+	return 0;
+}
